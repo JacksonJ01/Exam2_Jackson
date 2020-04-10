c9940642886016038081f1050b3ce4032d03e4a6
JacksonJ01 <jacksonj@hartwick.edu>
2020-04-10 19:39:41 -0400

    The Quick sort method along with the Merge method are easier for me to understand. All that "in range()" takes a little while to get used to

--- Search_Methods_Driver.py
+++ Search_Methods_Driver.py
@@ -1,1 +1,11 @@
+from Search_Methods import *
 
+choice = 'Start'
+while choice == 'Start':
+    print("Press the number of the first letter for the desired sort method ")
+    menu = input('     -MENU-'
+                 '\n1. BUBBLE'
+                 '\n2. INSERTION'
+                 '\n3. SELECTION'
+                 '\n4. MERGE'
+                 '\n5. QUICK').title()
